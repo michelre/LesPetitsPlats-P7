dad44da342a456d4217b18ce62be3948d56f0af5
Rémi Michel <remi.michel38@gmail.com>
2023-05-04 20:19:38 +0200

    feat(): Session du 04/05 — conseils

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,28 @@
-// Récupération des différentes recettes via un fetch 
+/* REMI: Ajout des variables globales tout en haut pour faciliter le futur algo de recherche */
+// Déclaration des variables
+let recipes = []
+
+// Récupération des différentes recettes via un fetch
 async function getRecipes() {
     const res = await fetch("../data/recipes.json")
     return await res.json()
 }
 
 async function init() {
-    const recipes = await getRecipes();
-    displayRecipes(recipes);
+    /* REMI: On peut stocker les recettes dans la variable globale prévue à cet effet */
+    recipes = await getRecipes();
+    /* REMI: Vu que recipes est une variable globale, pas besoin de la passer en paramètre à la fonction */
+    displayRecipes();
     console.log(recipes);
 };
 init();
 
-// Déclaration des variables
-let recipes = []
-
 // Déclaration des constantes
-const recipeSection = document.getElementById('cards-container');
+/* REMI: Duplication de la variable recipeSection non nécessaire ici de manière globale */
+//const recipeSection = document.getElementById('cards-container');
 
 // TEST - Affichage des Cards recette via la factory recipeFactory
-function displayRecipes(recipes) {
+function displayRecipes() {
     const recipeSection = document.getElementById('cards-container');
     recipeSection.innerHTML = ''
 
@@ -29,6 +33,14 @@ function displayRecipes(recipes) {
     })
 }
 
+
+/* REMI: Conseils:
+* 1. Faire un composant "Select" qui prend en paramètre les éléments (ingrédients / ustensils / appareils) à afficher
+* 2. Afficher les ingrédients / ustensils et appareils dans chacun des selects
+* 3. Rassembler les évènements dans une méthode propre à ce composant
+*   - Comme ci-dessous, au click sur le composant, l'ouvrir ou le fermer pour afficher ou cacher le contenu
+*   - Au click sur un élément de la liste remonter l'information de l'élément cliqué (on verra ça ensemble, c'est le plus chiant)
+*  */
 
 
 // TEST - ANIMATION AU CLIC LISTBOXS
