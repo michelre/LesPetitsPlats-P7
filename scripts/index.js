d13e5375b2794eeb037abc8cca5f6d4a51dcaa74
Graff Médéric <graffmederic@gmail.com>
2023-05-12 19:08:27 +0200

    Listboxs : Init composant Select. Récupération des datas fonctionnelle + Affichage et suppression Tags fonctionnelle

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,38 @@
-// Récupération des différentes recettes via un fetch 
+import Select from './factories/Select.js'
+
+/* REMI: Ajout des variables globales tout en haut pour faciliter le futur algo de recherche */
+// Déclaration des variables
+let recipes = []
+let selectedIngredients = []
+let selectedAppliances = []
+let selectedUstensiles = []
+
+const tags = document.querySelector('.tags')
+
+// Récupération des différentes recettes via un fetch
 async function getRecipes() {
     const res = await fetch("../data/recipes.json")
     return await res.json()
 }
 
 async function init() {
-    const recipes = await getRecipes();
-    displayRecipes(recipes);
-    console.log(recipes);
+    /* REMI: On peut stocker les recettes dans la variable globale prévue à cet effet */
+    recipes = await getRecipes();
+    /* REMI: Vu que recipes est une variable globale, pas besoin de la passer en paramètre à la fonction */
+    displayRecipes();
+    displayIngredientsSelect()
+    //TODO: A décommenter quand ingredient fonctionne
+    displayAppliancesSelect()
+    displayUstensilsSelect()
 };
 init();
 
-// Déclaration des variables
-let recipes = []
-
 // Déclaration des constantes
-const recipeSection = document.getElementById('cards-container');
+/* REMI: Duplication de la variable recipeSection non nécessaire ici de manière globale */
+//const recipeSection = document.getElementById('cards-container');
 
 // TEST - Affichage des Cards recette via la factory recipeFactory
-function displayRecipes(recipes) {
+function displayRecipes() {
     const recipeSection = document.getElementById('cards-container');
     recipeSection.innerHTML = ''
 
@@ -29,6 +43,94 @@ function displayRecipes(recipes) {
     })
 }
 
+function displayIngredientsSelect() {
+    const ingredients = recipes
+        .map(recipe =>
+            recipe.ingredients
+                .map(ingredient => ingredient.ingredient)
+        ).flat()
+    const select = new Select(
+        ingredients,
+        'ingredient',
+        'Ingrédient',
+        'Recherchez vos ingrédients',
+        (ingredient) => {
+            selectedIngredients.push(ingredient)
+            const listElement = document.createElement('li')
+            listElement.innerText = ingredient
+            tags.appendChild(listElement)
+            console.log(selectedIngredients)
+
+            listElement.addEventListener('click', () => {
+                selectedIngredients = selectedIngredients
+                    .filter(selectedIngredient => selectedIngredient !== ingredient)
+                listElement.remove()
+            })
+        }
+    )
+    const triIngredients = document.querySelector('.tri-ingredients')
+    triIngredients.appendChild(select.render())
+}
+
+function displayAppliancesSelect() {
+    const appliances = recipes.map(recipe => recipe.appliance)
+    const select = new Select(
+        appliances,
+        'appareil',
+        'Appareils',
+        'Recherchez vos appareils',
+        (appliance) => {
+            selectedAppliances.push(appliance)
+            const listElement = document.createElement('li')
+            listElement.innerText = appliance
+            tags.appendChild(listElement)
+            console.log(selectedAppliances)
+
+            listElement.addEventListener('click', () => {
+                selectedAppliances = selectedAppliances
+                    .filter(selectedAppliance => selectedAppliance !== appliance)
+                listElement.remove()
+            })
+        }
+    )
+    const triIngredients = document.querySelector('.tri-appareils')
+    triIngredients.appendChild(select.render())
+}
+
+function displayUstensilsSelect() {
+    const ustensils = recipes
+        .map(recipe => recipe.ustensils)
+        .flat()
+    const select = new Select(
+        ustensils,
+        'ustensil',
+        'Ustensiles',
+        'Recherchez vos ustensiles',
+        (ustensil) => {
+            selectedUstensiles.push(ustensil)
+            const listElement = document.createElement('li')
+            listElement.innerText = ustensil
+            tags.appendChild(listElement)
+            console.log(selectedUstensiles)
+
+            listElement.addEventListener('click', () => {
+                selectedUstensiles = selectedUstensiles
+                    .filter(selectedUstensil => selectedUstensil !== ustensil)
+                listElement.remove()
+            })
+        }
+    )
+    const triIngredients = document.querySelector('.tri-ustensiles')
+    triIngredients.appendChild(select.render())
+}
+
+/* REMI: Conseils:
+* 1. Faire un composant "Select" qui prend en paramètre les éléments (ingrédients / ustensils / appareils) à afficher
+* 2. Afficher les ingrédients / ustensils et appareils dans chacun des selects
+* 3. Rassembler les évènements dans une méthode propre à ce composant
+*   - Comme ci-dessous, au click sur le composant, l'ouvrir ou le fermer pour afficher ou cacher le contenu
+*   - Au click sur un élément de la liste remonter l'information de l'élément cliqué (on verra ça ensemble, c'est le plus chiant)
+*  */
 
 
 // TEST - ANIMATION AU CLIC LISTBOXS
@@ -37,12 +139,12 @@ function displayRecipes(recipes) {
 var bouton = document.querySelector('.tri-ingredients button');
 
 // Ajoute un écouteur d'événement pour détecter le clic sur le bouton
-bouton.addEventListener('click', function() {
+bouton.addEventListener('click', function () {
 
 // Récupère l'élément de la div parente du bouton
-var divParente = bouton.parentNode;
+    var divParente = bouton.parentNode;
 
 // Ajoute une classe à la div parente pour agrandir sa largeur et afficher l'input de recherche
-divParente.classList.toggle('ouvert');
+    divParente.classList.toggle('ouvert');
 
 });
